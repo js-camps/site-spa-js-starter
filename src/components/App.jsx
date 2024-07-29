d13415f40b7cc9e3d27e06d08aa7c21f27a574f8
rjgeng <rj.geng@gmail.com>
2024-07-29 08:01:03 -0700

    adds a custom React component example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import FormComponent from "./FormComponent";
 
 function App() {
-  return (
-    <div>
-      Welcome to Sites
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to Labs Basic SPA</h1>
+            <p>
+                The following are a few components put together in a way that informs
+                industry practices. Use this repository as a boilerplate for your work.
+                Follow the file architecture and component patterns you see here.
+            </p>
+            <div>
+                <p>
+                    This is an example of a common form input group. See the
+                    `./src/components/FormComponents` directory for more information.
+                    Notice that we&apos;re importing the entire form group from the `index.js`
+                    file found within that directory. This pattern will help you break
+                    down your features in terms of components. Once you&apos;re read, organize
+                    your components inside a &apos;container&apos; and simply use the `index.js`
+                    file to pass that container around in your app.
+                </p>
+                <FormComponent />
+            </div>
+        </div>
+    );
 }
 
 export default App;
