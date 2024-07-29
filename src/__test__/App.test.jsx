73be39c1757236fa9b856790cff07fabe0f759c1
rjgeng <rj.geng@gmail.com>
2024-07-29 06:02:55 -0700

    adds boiler

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from '../components/App.jsx';
 
 describe('App Component', () => {
     it('renders welcome message', () => {
