3a6e34d9032b2c9ef770da088fcb926d7e25ad39
rjgeng <rj.geng@gmail.com>
2024-07-29 14:57:21 -0700

    adds (okta) login authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Routers from './components/Routers.jsx'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Login from "./components/LoginComponent";
+import Home from "./components/HomeComponent";
+
+// import { Security, LoginCallback } from "@okta/okta-react";
+// import { config } from "./utils/oktaConfig";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <React.StrictMode>
-        <Routers />
+          {/*<Security {...config}>*/}
+              <Route path="/" exact>
+                  <Home />
+              </Route>
+              <Route path="/login">
+                  <Login />
+              </Route>
+              {/*<Route path="/implicit/callback" component={LoginCallback} />*/}
+          {/*</Security>*/}
       </React.StrictMode>
     </BrowserRouter>,
 )
