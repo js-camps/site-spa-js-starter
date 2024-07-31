7ae88aead77bc61fda6f3ae4eb4b6ab5ce7ebde0
rjgeng <rj.geng@gmail.com>
2024-07-31 09:33:04 -0700

    Renames Routes -> Nav

---
+++ src/components/Nov.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+            </nav>
+        </>
+    );
+};
+export default Nav;
+
