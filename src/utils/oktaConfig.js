c149f4cab50529bcf278c49f120d04e919a06423
rjgeng <rj.geng@gmail.com>
2024-08-02 05:27:42 -0700

    added env var REACT_APP_OKTA_ISSUER_URI

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,9 +1,7 @@
 import process from '../../.eslintrc.cjs';
 
-const OktaURL = 'dev-886474.okta.com';
-
 const config = {
-  issuer: `https://${OktaURL}/oauth2/default`,
+  issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
   redirectUri: window.location.origin + '/implicit/callback',
   clientId: process.env.REACT_APP_CLIENT_ID,
   pkce: true,
