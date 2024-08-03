29119f92d3d7e050fd65a1df443c8e8e39581b69
rjgeng <rj.geng@gmail.com>
2024-08-03 07:21:26 -0700

    updates homepage messaging and readme

--- src/components/pages/Home/RenderHomePage.jsx
+++ src/components/pages/Home/RenderHomePage.jsx
@@ -3,28 +3,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function RenderHomePage(props) {
-  // eslint-disable-next-line react/prop-types
-  const { userInfo } = props;
+  const {
+    // eslint-disable-next-line react/prop-types
+    userInfo,
+    // authService
+  } = props;
   return (
     <div>
       <h1>Welcome to Labs Basic SPA</h1>
       {/* eslint-disable-next-line react/prop-types */}
       <h3>Hi {userInfo.name}, you are more than welcome!</h3>
-      <p>
-        The following are a few components put together in a way that informs
-        industry practices. Use this repository as a boilerplate for your work.
-        Follow the file architecture and component patterns you see here.
-      </p>
       <div>
         <p>
-          This is an example of a common form input group. See the
-          `./src/components/FormComponents` directory for more information.
-          Notice that we&lsquo;re importing the entire form group from the
-          `index.js` file found within that directory. This pattern will help
-          you break down your features in terms of components. Once you&lsquo;re
-          read, organize your components inside a &lsquo;container&lsquo; and
-          simply use the `index.js` file to pass that container around in your
-          app.
+          This is an example of a common example of how we'd like for you to
+          approach components.
         </p>
         <p>
           <Link to="/profile-list">Profiles</Link>
@@ -43,3 +35,4 @@ function RenderHomePage(props) {
   );
 }
 export default RenderHomePage;
+
