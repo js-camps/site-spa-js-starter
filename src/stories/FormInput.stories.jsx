a8e3751cbb6ce2ba6ce98ab8f8ee9aaba9c5fa81
rjgeng <rj.geng@gmail.com>
2024-08-06 08:37:21 -0700

    Adds Ant Design Input To Storybook

--- src/stories/FormInput.stories.jsx
+++ src/stories/FormInput.stories.jsx
@@ -1,9 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { FormInput } from '../components/common';
+import { Input } from 'antd';
+
+import 'antd/dist/antd.css';
 
 export default { title: 'Form Input' };
 
 export const basicUsage = () => (
   <FormInput placeholder="Form Input" name="formInput" labelId="Form Input" />
+);
+
+export const usageWithAnt = () => (
+  <Input placeholder="Form Input" addonBefore="Form Input" />
 );
